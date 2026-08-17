0329ab99803b63e61244ea9223d3930f403f810f
amirshafie <amirshafie738@gmail.comgit config --global user.name amirshafiegit config --global user.email amirshafie738@gmail.com>
2026-08-17 17:57:43 +0330

    feat: creat mover cart component

--- src/components/MovieCart.tsx
+++ src/components/MovieCart.tsx
@@ -1,14 +1,65 @@
+import { Heart, Pencil, Star, Trash2 } from "lucide-react";
 import type { Imovie } from "../types/movie";
 interface MovieCartProps {
-    movie: Imovie;
+  movie: Imovie;
+  index:number;
 }
 
+function MovieCart({ movie,index }: MovieCartProps) {
+  return (
+    <>
+      
+      <tbody>
+            <tr key={movie.id} className="border-b border-gray-200">
+            <td className="p-4">{index+1}</td>
+              <td className="p-4">
+                <div className="flex items-center gap-4">
+                  <img
+                    src={movie.image}
+                    alt={movie.title}
+                    className="h-24 w-16 rounded object-cover"
+                  />
 
-function MovieCart({movie}:MovieCartProps){
-    return(
-        <>
-            <h3>{movie.title}</h3>
-        </>
-    )
+                  <div className="max-w-xs">
+                    <h2 className="font-semibold">{movie.title}</h2>
+
+                    <p className="mt-2 text-sm text-gray-500">
+                      {movie.description}
+                    </p>
+                  </div>
+                </div>
+              </td>
+
+              <td className="p-4 text-sm">{movie.genre}</td>
+
+              <td className="p-4 text-sm">{movie.year}</td>
+
+              <td className="p-4">
+                <div className="flex items-center gap-1">
+                  <Star size={16} className="fill-yellow-400 text-yellow-400" />
+
+                  <span>{movie.rating}</span>
+                </div>
+              </td>
+
+              <td className="p-4">
+                <div className="flex gap-2">
+                  <button className="rounded border p-2 cursor-pointer">
+                    <Heart size={18} />
+                  </button>
+
+                  <button className="rounded border p-2 cursor-pointer">
+                    <Pencil size={18} />
+                  </button>
+
+                  <button className="rounded border p-2 text-red-500 cursor-pointer">
+                    <Trash2 size={18} />
+                  </button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+    </>
+  );
 }
-export default MovieCart
+export default MovieCart;
--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -4,29 +4,43 @@ import type { Imovie } from "../types/movie";
 import MovieCart from "./MovieCart";
 
 function MovieList() {
-    const {
-        data: movies,
-        isLoading,
-        isError,
-        error,
-    } = useQuery({
-        queryKey: ["movies"],
-        queryFn: getMovies,
-    });
-    if (isLoading) {
-        return <p>Loading</p>;
-    }
+  const {
+    data: movies,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["movies"],
+    queryFn: getMovies,
+  });
+  if (isLoading) {
+    return <p>Loading</p>;
+  }
 
-    if (isError) {
-        return <p>{error.message}</p>;
-    }
-    return (
-        <>
-            {movies.map((movie: Imovie) => (
-                <MovieCart key={movie.id} movie={movie}/>
-            ))}
-        </>
-    );
+  if (isError) {
+    return <p>{error.message}</p>;
+  }
+  return (
+    <>
+      <div className="w-full overflow-x-auto">
+        <table className="w-full border-collapse">
+          <thead>
+            <tr className="border-b text-left text-sm text-gray-500">
+            <th className="p-4">#</th>
+              <th className="p-4">Title</th>
+              <th className="p-4">Genre</th>
+              <th className="p-4">Year</th>
+              <th className="p-4">Rating</th>
+              <th className="p-4">Actions</th>
+            </tr>
+          </thead>
+          {movies.map((movie: Imovie,index:number) => (
+            <MovieCart key={movie.id} movie={movie} index={index}/>
+          ))}
+        </table>
+      </div>
+    </>
+  );
 }
 
 export default MovieList;
