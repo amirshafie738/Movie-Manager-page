638c0c07437e07e12915ba43c4ea5cffa18d9f98
amirshafie <amirshafie738@gmail.comgit config --global user.name amirshafiegit config --global user.email amirshafie738@gmail.com>
2026-08-20 19:41:57 +0330

    feat:creat mutation

--- src/services/api/movie.tsx
+++ src/services/api/movie.tsx
@@ -1,6 +1,19 @@
+import { useMutation } from "@tanstack/react-query";
+import type { Imovie } from "../../types/movie";
+import type { MovieFormData } from "../../validation/movieSchema";
 import { api } from "./axios";
 
 export async function getMovies() {
   const response = await api.get("/movies");
   return response.data;
+}
+
+export function usePostMovie() {
+  return useMutation({
+    mutationFn: async (movie: MovieFormData): Promise<Imovie> => {
+      
+      const response = await api.post<Imovie>("/movies", movie);
+      return response.data;
+    },
+  });
 }
