749be44fa4df92a7f2bc500f72e4b584e251b8eb
amirshafie <amirshafie738@gmail.comgit config --global user.name amirshafiegit config --global user.email amirshafie738@gmail.com>
2026-08-22 22:41:26 +0330

    feat:creat redux-presist

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,33 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import movieReducer from "../redux/reducers/TodoCartRaducer";
 import favoriteRaducer from "../redux/reducers/favoriteSlice";
+import storage  from 'redux-persist/es/storage';
+import { FLUSH, PAUSE, PURGE, REHYDRATE, persistReducer } from 'redux-persist';
+import persistStore from "redux-persist/es/persistStore";
 
-export const store = configureStore({
-    reducer:{ 
-         movie: movieReducer,
-         favorites:favoriteRaducer,
-    }
+const combineRaducer =combineReducers({
+    movie: movieReducer,
+    favorites:favoriteRaducer,
 })
+
+const presistConfig ={
+    key:"favorite",
+    storage,
+    whitelist:['favorites']
+}
+
+const persistedReducer = persistReducer(presistConfig,combineRaducer)
+
+export const store =configureStore({
+    reducer:persistedReducer,
+    middleware:(getDefaultMiddleware)=>
+      getDefaultMiddleware({
+        serializableCheck:{
+          ignoredActions:[FLUSH,REHYDRATE,PAUSE,PURGE]
+        }
+      })
+  })
+  export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
