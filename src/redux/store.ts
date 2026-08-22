75336c5e9066604c041cd9fc309b631c11b72793
amirshafie <amirshafie738@gmail.comgit config --global user.name amirshafiegit config --global user.email amirshafie738@gmail.com>
2026-08-22 22:16:57 +0330

    feat:add favorite function with redux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import movieReducer from "../redux/reducers/TodoCartRaducer";
+import favoriteRaducer from "../redux/reducers/favoriteSlice";
 
 export const store = configureStore({
-    reducer:{  movie: movieReducer,}
+    reducer:{ 
+         movie: movieReducer,
+         favorites:favoriteRaducer,
+    }
 })
 
 export type RootState = ReturnType<typeof store.getState>;
