6fc6a60f85fc56619138cabd4c962e5ba837a873
amirshafie <amirshafie738@gmail.comgit config --global user.name amirshafiegit config --global user.email amirshafie738@gmail.com>
2026-08-23 12:17:09 +0330

    feat:creat toast

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { Provider } from "react-redux";
 import { persistor, store } from "./redux/store.ts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PersistGate } from "redux-persist/integration/react";
-
+import { Toaster } from "react-hot-toast";
 export const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
@@ -19,6 +19,7 @@ createRoot(document.getElementById("root")!).render(
             <App />
           </PersistGate>
         </Provider>
+        <Toaster position="top-right" reverseOrder={false} />
       </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
