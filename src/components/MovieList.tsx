42e07f975373efed692571d502d5224ba09deb14
amirshafie <amirshafie738@gmail.comgit config --global user.name amirshafiegit config --global user.email amirshafie738@gmail.com>
2026-09-01 21:25:23 +0330

    feat:creat pagination

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -2,21 +2,19 @@ import { useQuery } from "@tanstack/react-query";
 import { getMovies } from "../services/api/movie";
 import type { Imovie } from "../types/movie";
 import MovieCart from "./MovieCart";
+import { useState } from "react";
 interface MovieFormProps {
-  setIsFormOpen:React.Dispatch<React.SetStateAction<boolean>>;
-  search:string;
-  genre:string;
+  setIsFormOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  search: string;
+  genre: string;
 }
-function MovieList({setIsFormOpen,search,genre}:MovieFormProps) {
+function MovieList({ setIsFormOpen, search, genre }: MovieFormProps) {
+  // صفحات پیج
+  const [page, setPage] = useState(1);
 
-  const {
-    data:movies,
-    isLoading,
-    isError,
-    error,
-  } = useQuery({
-    queryKey: ["movies", search,genre],
-    queryFn: () => getMovies({ search,genre }),
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["movies", search, genre, page],
+    queryFn: () => getMovies({ search, genre, page, limit: 5 }),
   });
   if (isLoading) {
     return <p>Loading</p>;
@@ -25,13 +23,21 @@ function MovieList({setIsFormOpen,search,genre}:MovieFormProps) {
   if (isError) {
     return <p>{error.message}</p>;
   }
+
+  //فیلم‌های صفحه فعلی
+
+  const movies: Imovie[] = data?.data ?? [];
+
+  // تعداد کل صفحات
+
+  const totalPages = data?.pages ?? 1;
   return (
-    <>
+    <div className="flex flex-col gap-5 w-full overflow-x-auto">
       <div className="w-full overflow-x-auto">
         <table className="w-full border-collapse">
           <thead>
             <tr className="border-b text-left text-sm text-gray-500">
-            <th className="p-4">#</th>
+              <th className="p-4">#</th>
               <th className="p-4">Title</th>
               <th className="p-4">Genre</th>
               <th className="p-4">Year</th>
@@ -39,12 +45,54 @@ function MovieList({setIsFormOpen,search,genre}:MovieFormProps) {
               <th className="p-4">Actions</th>
             </tr>
           </thead>
-          {movies.map((movie: Imovie,index:number) => (
-            <MovieCart key={movie.id} movie={movie} index={index} setIsFormOpen={setIsFormOpen}/>
+          {movies.map((movie: Imovie, index: number) => (
+            <MovieCart
+              key={movie.id}
+              movie={movie}
+              index={index}
+              setIsFormOpen={setIsFormOpen}
+            />
           ))}
         </table>
       </div>
-    </>
+      {/* Pagination */}
+      <div className="flex justify-center items-center gap-2 mt-6">
+        {/* صفحه قبل */}
+        <button
+          onClick={() => setPage((prev) => prev - 1)}
+          disabled={page === 1}
+          className="px-4 py-2 rounded-lg border disabled:opacity-40 disabled:cursor-not-allowed hover:bg-gray-100"
+        >
+          Previous
+        </button>
+
+        {/* شماره صفحات */}
+        {Array.from({ length: totalPages }, (_, index) => index + 1).map(
+          (pageNumber) => (
+            <button
+              key={pageNumber}
+              onClick={() => setPage(pageNumber)}
+              className={`w-10 h-10 rounded-lg border ${
+                page === pageNumber
+                  ? "bg-blue-400 text-white"
+                  : "hover:bg-gray-100"
+              }`}
+            >
+              {pageNumber}
+            </button>
+          )
+        )}
+
+        {/* صفحه بعد */}
+        <button
+          onClick={() => setPage((prev) => prev + 1)}
+          disabled={page === totalPages}
+          className="px-4 py-2 rounded-lg border disabled:opacity-40 disabled:cursor-not-allowed hover:bg-gray-100"
+        >
+          next
+        </button>
+      </div>
+    </div>
   );
 }
 
--- src/services/api/movie.tsx
+++ src/services/api/movie.tsx
@@ -5,11 +5,15 @@ import { api } from "./axios";
 interface GetMoviesParams {
   search?: string;
   genre?: string;
+  page?: number;
+  limit?: number;
 }
 
 export async function getMovies({
   search = "",
   genre = "",
+  page = 1,
+  limit = 5,
 }: GetMoviesParams) {
   const params: Record<string, string> = {};
 
@@ -21,17 +25,21 @@ export async function getMovies({
     params.genre = genre;
   }
 
+  params["_page"] = String(page);
+  params["_per_page"] = String(limit);
+
   const response = await api.get("/movies", {
     params,
   });
 
   return response.data;
 }
+
 export function usePostMovie() {
   return useMutation({
     mutationFn: async (movie: MovieFormData): Promise<Imovie> => {
-      
       const response = await api.post<Imovie>("/movies", movie);
+
       return response.data;
     },
   });
